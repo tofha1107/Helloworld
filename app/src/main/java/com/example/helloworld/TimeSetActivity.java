024c54d84c6a025d4d486e8cdf14642a52646033
tofha1107 <romzoa@naver.com>
2020-10-14 11:29:58 +0900

    새롬 오전 11:29

--- app/src/main/java/com/example/helloworld/TimeSetActivity.java
+++ app/src/main/java/com/example/helloworld/TimeSetActivity.java
@@ -5,18 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 import java.io.Serializable;
 
-public class TimeSetActivity extends AppCompatActivity implements Serializable{
+public class TimeSetActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button time_next_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.time_set);
-        Log.v("info_success",LoginInfo.info.getName());
+
+        time_next_button = (Button) findViewById(R.id.time_next_button);
+        time_next_button.setOnClickListener(this);
+        //Log.v("info_success",LoginInfo.info.getName());
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.time_next_button){
+            Intent intent = new Intent(this, DistanceSetActivity.class);
+            startActivity(intent);
+        }
+    }
 }
