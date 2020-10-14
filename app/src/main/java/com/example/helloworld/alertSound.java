ce409b9f496831ae8f1909dc0d0259511f45d066
naneunsz <naneunsz@gmail.com>
2020-10-14 17:38:26 +0900

    1014 16:13, 오디오 진행중, 변화없음

--- app/src/main/java/com/example/helloworld/alertSound.java
+++ app/src/main/java/com/example/helloworld/alertSound.java
@@ -6,24 +6,15 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class alertSound extends AppCompatActivity implements View.OnClickListener {
-
-    private final static int REQUESTCODE_RINGTONE_PICKER = 1000;
-
-    // 문제 해결을 위해 Ringtone으로 변경
-    private RingtoneManager mRtm;
-    // 현재 재생중인 링톤
-    private Ringtone mRtCurrent;
+public class alertSound extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_sound);
-    }
 
-    @Override
-    public void onClick(View view) {
 
     }
 }
